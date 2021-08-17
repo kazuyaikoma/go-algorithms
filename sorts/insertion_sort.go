16798742cedefdcd9f8e8c5f39e972ec52e52e56
nixiesquid <audu817@gmail.com>
2021-08-17 11:32:04 +0900

    add insertion_sort

---
+++ sorts/insertion_sort.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func insertionSort(a []int) []int {
+	for i := 1; i < len(a); i++ {
+		for j := i - 1; 0 <= j; j-- {
+			if a[j+1] < a[j] {
+				a[j+1], a[j] = a[j], a[j+1]
+			} else {
+				break
+			}
+		}
+	}
+
+	return a
+}
+
+func main() {
+	a := []int{2, 3, 4, 1, 5}
+	fmt.Println(insertionSort(a))
+}
